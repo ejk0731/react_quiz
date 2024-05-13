66877639ddfbf73af39245d2f1ee17eefc55d39a
eunji <103646256+ejk0731@users.noreply.github.com>
2024-05-13 18:25:11 +0900

    fix: [Quiz 02] canvas, IntersectionObserver 적용 테스트

--- src/modules/Canvas.tsx
+++ src/modules/Canvas.tsx
@@ -8,58 +8,74 @@ const cx = classNames.bind(styles);
 
 function Canvas() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const [scroll, setScroll] = useState(0);
   const [rotateDeg, setRotateDeg] = useState<number>(0);
   const [isRotate, setIsRotate] = useState(false);
 
-  const handleScroll = () => {
-    setScroll(window.scrollY);
-  };
+  // useEffect(() => {
+  //   const scrollY = window.scrollY;
+  //   const screenHeight = document.body.offsetHeight;
+  //   const main = document.getElementById("canvas") as HTMLElement;
+  //   // const mainPos = Math.floor(scrollY + main.getBoundingClientRect().top);
+  //   const mainPos = main.getBoundingClientRect().top;
+  //   // console.log(scrollY, mainPos)
+  //   window.addEventListener("wheel", (e) => {
+  //     // if (isRotate) {
+  //       if (e.deltaY > 0) setRotateDeg(rotateDeg + 10);
+  //       if (e.deltaY < 0) setRotateDeg(rotateDeg - 10);
+  //     // }
+  //     setIsRotate(
+  //       mainPos >= scrollY ||
+  //       // screenHeight >= mainPos > 0 || scrollY < mainPos + main.clientHeight
+  //     );
+  //   });
+
+  //   console.log('여긴언제?', rotateDeg, isRotate)
+
+  //   if (canvasRef.current) {
+  //     // Q. 여기는 정해져 있는데 왜 useEffect 안에서 써야할까?
+  //     // Q. current는 어떠한 function 안에서 작동??
+  //     const canvas = canvasRef.current;
+  //     canvas.width = 300;
+  //     canvas.height = 100;
+  //     canvas.style.background = `url(${mapBackground})`;
+  //     canvas.style.rotate = `${rotateDeg}deg`;
+  //     //
+  //   }
+  // }, [canvasRef, rotateDeg, window]);
 
   useEffect(() => {
-    const scrollY = window.scrollY;
-    const screenHeight = document.body.offsetHeight;
-    const main = document.getElementById("canvas") as HTMLElement;
-    const footer = document.getElementById("footer") as HTMLElement;
-
-    const mainPos = Math.floor(scrollY + main.getBoundingClientRect().top);
-    const footerPos = Math.floor(scrollY + footer.getBoundingClientRect().top);
-
-    setIsRotate(
-      screenHeight >= mainPos &&
-        scrollY < mainPos + main.clientHeight
+    // window.scrollTo(0, 0);
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          // true 일때 간격......을?????? 두고 회전
+          console.log("보인다보여");
+          window.addEventListener("wheel", (e) => {
+            if (e.deltaY > 0) setRotateDeg(rotateDeg + 10);
+            if (e.deltaY < 0) setRotateDeg(rotateDeg - 10);
+          });
+        } else {
+        }
+      },
+      { threshold: 0.01 }
     );
-    console.log(isRotate, rotateDeg);
-
+    
     if (canvasRef.current) {
-      // Q. 여기는 정해져 있는데 왜 useEffect 안에서 써야할까?
-      // Q. current는 어떠한 function 안에서 작동??
       const canvas = canvasRef.current;
-      // const ctx = canvasRef.current.getContext(
-      //   "2d"
-      // ) as CanvasRenderingContext2D;
       canvas.width = 300;
       canvas.height = 100;
       canvas.style.background = `url(${mapBackground})`;
-      //
-      if (isRotate) {
-        console.log("main 맞음");
-        setRotateDeg(rotateDeg + 10);
-        canvas.style.rotate = `${rotateDeg}deg`;
-      } else {
-        console.log("main 아님");
-      }
-
-      window.addEventListener("scroll", handleScroll);
-      return () => {
-        window.removeEventListener("scroll", handleScroll); //clean up
-      };
-
-      // 1. 스크롤시 돌아감
-      // 2. 스크롤 정도에 따라 동일하게 돌아가도록
-      // 3. 다시 맨 위로 오면 원래 각도로 돌아가도록
+      observer.observe(canvasRef.current);
+      canvasRef.current.style.rotate = `${rotateDeg}deg`;
     }
-  }, [canvasRef, scroll]);
+
+    // return () => {
+    //   if (canvasRef.current) {
+    //     observer.unobserve(canvasRef.current);
+    //     console.log("안보여~")
+    //   }
+    // };
+  }, [rotateDeg]);
 
   return (
     <Layout>
